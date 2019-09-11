aaadfb464123f1a61e104af0649e137c1358bfcf
luzagm <luz.aguirre.mo@gmail.com>
2019-09-11 15:48:16 +0200

    add pokemon names, images and powers

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "../stylesheet/App.css";
 import PokeList from "./PokeList";
-import Pokemon from "./Pokemon";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
+      <PokeList />
     </div>
   );
 }
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 class Pokemon extends React.Component {
+  render() {
+    let pokemonPowersList = this.props.pokemonPower.map(power => {
+      return <li>{power}</li>;
+    });
 
-    render () {
-return ()
-    }
+    return (
+      <div>
+        <img src={`${this.props.pokemonImage}`}></img>
+        <h2>{this.props.pokemonName}</h2>
+        <ul>{pokemonPowersList}</ul>
+      </div>
+    );
+  }
 }
 
 export default Pokemon;
