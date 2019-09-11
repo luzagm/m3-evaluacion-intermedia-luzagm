3ebdefe7f604edaeb4c0557e2b8d815a4c1a4553
luzagm <luz.aguirre.mo@gmail.com>
2019-09-11 16:53:29 +0200

    build

---
+++ docs/build/precache-manifest.153da61a222846f91b996a2732ab850a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f17f64e01143b7d688e30a001da7af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3f9157c0e73848274b6e",
+    "url": "./static/css/main.b98ccfdf.chunk.css"
+  },
+  {
+    "revision": "e4c85652eab9bfc8a3bb",
+    "url": "./static/js/2.0cd421da.chunk.js"
+  },
+  {
+    "revision": "3f9157c0e73848274b6e",
+    "url": "./static/js/main.62adbf89.chunk.js"
+  },
+  {
+    "revision": "5a1c1f2095aa209ed73a",
+    "url": "./static/js/runtime~main.d5b79812.js"
+  }
+]);
